334ccb742a8ac84ec0cb1db7ef799ecd8b3b3a65
Wyatt Waterfield <waterfield.wyatt@gmail.com>
2016-03-29 13:53:21 -0400

    Created loop to save the board.

--- Connect4/ourplayer.c
+++ Connect4/ourplayer.c
@@ -7,9 +7,25 @@
 int moving(const struct connect4 *game, int secondsLeft)
 {
     int i;
+
+    char boardCopy[NUM_ROWS][NUM_COLS];
+    /*
     for(i=0;i<NUM_COLS;i++)
     {
         if(game->board[NUM_ROWS-1][i] == '_')
             return i;
+    } */
+
+    // Get a copy of the  current board.
+    for (i = 0; i < NUM_ROWS; i++)
+    {
+        for (j = 0; j < NUM_COLS; j++)
+        {
+            boardCopy[i][j] = game->board[i][j];
+        }
     }
+
+
+
+
 }
