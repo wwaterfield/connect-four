08e6d46bd0b8a082ea3149e1947d752b431b91eb
Areax <actuallythisisspam@gmail.com>
2016-03-30 22:19:04 -0400

    update

--- Connect4/ourplayer.c
+++ Connect4/ourplayer.c
@@ -6,63 +6,16 @@
 #define LOSE -1000;
 #define WIN 1000;
 #define COL_SIZE 7;
+#define RUNS 4;
 
-int checkscore(char board[6][7], char team)
+int checkscore(const struct connect4* game)
 {
-    int us, i, j;
+    int i, j;
 
-    //this is just knowing what player we are???
-    if(team == PLAYERONE)
-        us = 1;
-    else us = 0;
-    //I have no way to decide what player otherwise
     int tscore = -1000;
     int highscore;
     int yPos;
-    for(j=0;j<7;j++)
-    {
-        tscore = score(board, j);
 
-        if(total <= tscore)
-        {
-            yPos = j;
-            highscore = tscore;
-        }
-
-
-        }
-    }
-}
-
-<<<<<<< HEAD
-int score (char board[6][7], posInfo temp)
-=======
-int score (char board[6][7], char team, connect4 *game)
->>>>>>> bf262e00e2b3a3d0b3490faa9ac5199d1514a159
-{
-    int total;
-    int boardStatus;
-
-    boardStatus
-
-
-    // If all four are in a rows
-
-
-    return total;
-}
-
-int chk_status(char board[][NUM_COLS])
-{
-    return 0;
-}
-
-int moving(const struct connect4 *game, int secondsLeft)
-{
-    //here we have to make sure that if score == LOSE Then we MUST place a piece there. Same for win.
-    int i = 0;
-
-    int bestMove;
     char boardCopy[NUM_ROWS][NUM_COLS];
 
     // Get a copy of the  current board.
@@ -74,7 +27,67 @@ int moving(const struct connect4 *game, int secondsLeft)
         }
     }
 
-    bestMove = checkscore(boardCopy[][7], team);
+
+    for(j=0;j<7;j++)
+    {
+        tscore = score(boardCopy, j, team, game->whoseTurn);
+        if(total <= tscore)
+        {
+            yPos = j;
+            highscore = tscore;
+        }
+    }
+}
+
+int score (char board[6][7], int column, char tpiece, char ourpiece)
+{
+    int total = -1000;
+    board[get_row(&board, column)][column] = tpiece;
+    int status = check_status(&board);
+    //checks four cases: if we placed the piece and we win or not
+    //the win case will trigger when we place a piece
+    //the lose case will trigger when the hypothetical opponent places a piece
+    if(status == X_WINS && ourpiece == tpiece)
+        return WIN;
+    else if(status == X_WINS && ourpiece != tpiece)
+        return LOSE;
+    else if(status == O_WINS && ourpiece == tpiece)
+        return WIN;
+    else if(status == O_WINS && ourpiece != tpiece)
+        return LOSE;
+
+    //if() here and see if we get 2 pieces! high-ish score
+    //make sure that if we go in the middle we get more points
+    //if() here and see if we get 3 pieces! higher score
+    //if() here and see if we get 1 lonely piece.  Low score
+
+    //we might split up scoring into the opponent's scoring feature and ours.  That way
+    //we can go ahead and have 2 if statements per move...
+    //either one works?
+
+    return total;
+}
+
+int test_depth(const struct connect4 *game)
+{
+    int i;
+    int bestmove;
+    for(i=0;i<RUNS;i++)
+    {
+        bestmove = checkscore(game);
+    }
+
+    return bestmove;
+}
+
+int moving(const struct connect4 *game, int secondsLeft)
+{
+    //here we have to make sure that if score == LOSE Then we MUST place a piece there. Same for win.
+    int i = 0;
+
+    int bestMove;
+
+    bestMove = test_depth(game);
 
     return bestMove;
 }
--- Connect4/ourplayer.h
+++ Connect4/ourplayer.h
@@ -2,7 +2,7 @@
 #include "con4lib.h"
 #endif // CON4
 
-int moving(const struct connect4 *game, int secondsleft);
-int chk_status(char board[][NUM_COLS]);
-posInfo* checkscore(char board[6][7], char team)
-int score (char board[6][7], posInfo* node)
+int checkscore(const struct connect4* game);
+int score (char board[6][7], int column, char team);
+int test_depth(const struct connect4 *game);
+int moving(const struct connect4 *game, int secondsLeft);
