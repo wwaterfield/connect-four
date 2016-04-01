37c62d4102b0b5d6252db98bdb774f4d7795ca6f
Areax <actuallythisisspam@gmail.com>
2016-04-01 13:56:40 -0400

    BROKEN WORKING

--- Connect4/ourplayer.c
+++ Connect4/ourplayer.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "con4lib.h"
 #include "ourplayer.h"
+
 int checkscore(struct connect4* copy, char team)
 {
     int j;
--- Connect4/ourplayer.h
+++ Connect4/ourplayer.h
@@ -1,11 +1,15 @@
 #ifndef CON4
 #include "con4lib.h"
+
+#define LOSE -1000
+#define WIN 1000
+#define COL_SIZE 7
+#define RUNS 4
+
+
 #endif // CON4
 
-#define LOSE -1000;
-#define WIN 1000;
-#define COL_SIZE 7;
-#define RUNS 4;
+
 
 int checkscore(struct connect4* copy, char team);
 int findscore (struct connect4* copy, int column, char ourpiece);
