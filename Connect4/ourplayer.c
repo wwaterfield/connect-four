fadc84f772a74c5f1606a99cc31cd8262f95bb1d
Areax <actuallythisisspam@gmail.com>
2016-04-22 01:39:21 -0400

    Removed most of the debug statements

--- Connect4/ourplayer.c
+++ Connect4/ourplayer.c
@@ -3,17 +3,9 @@
 #include <math.h>
 #include "ourplayer.h"
 
-#define DEBUG 0
-
 
 BScore g9_checkscore(struct connect4* copied, BScore BestScore, int k)
 {
-    //1. accidentally hard coded ternary operators
-    //2. at the end of our move, check to see if we automatically lose
-    //if we lose, just go there instead? see test_depth, check for opposite, and bam
-    //3. I'm still not sure why it's glitching, but we can minmax tree it up
-    //4. we're having issues with finding easy wins, and although 2. may work
-    // looking for a long term option may be better
 
 	// Declaring Variables.
     int column;
@@ -23,20 +15,6 @@ BScore g9_checkscore(struct connect4* copied, BScore BestScore, int k)
     BScore finalsay = g9_initstruct(copied);
     finalsay.score = BestScore.score;
     finalsay.column = BestScore.column;
-
-    /* Jasper, what is going on here? */
-    //finalsay needs to be equal to the first instance of the minmaxtree
-    //     0
-    //    /a \b
-    //   /\  /\
-    // in this example, finalsay in a needs to be equal to the left branch, then the right branch will be
-    // compared in k!= DEPTH
-    //but I'm almost certain something's wrong here
-    //it's comparing several numbers and looking for a least better move or something,
-    //??? Check the debug statements and look for "This is the best column"
-    //it's the end of the recursion and shows you the best move
-    //for that particular branch. I can show you Monday
-    //but if we can fix that first that would be cool
 
     if(!turnour)
         finalsay.score = -3000;
@@ -59,17 +37,10 @@ BScore g9_checkscore(struct connect4* copied, BScore BestScore, int k)
 
         int fakescore = g9_findscore(copied, BestScore);
 
-        if(DEBUG)
-            printf("k: %d projected score: %d BestScore before addition: %d\n", k, fakescore, BestScore.score);
         BestScore.score += fakescore;
 
         if(k == DEPTH)
         {
-            if(DEBUG)
-                printf("score: %d  vs BestScore: %d, turn: %c\n", finalsay.score, BestScore.score, (turnour == 0) ? 'O' : 'X');
-            if(DEBUG)
-                printf("column: %d  vs Comparecol: %d, turn: %c\n", finalsay.column, BestScore.column, (turnour == 0) ? 'O' : 'X');
-
             // If it is the other player's turn, assign finalSay the most negative score.
             if(finalsay.score < fakescore && turnour == 0)
             {
@@ -83,9 +54,6 @@ BScore g9_checkscore(struct connect4* copied, BScore BestScore, int k)
                 finalsay.score = BestScore.score;
                 finalsay.column = BestScore.column;
             }
-
-            if(DEBUG)
-                printf("final score now: %d best column: %d turn: %c\n\n", finalsay.score, finalsay.column, (turnour == 0) ? 'O' : 'X');
         }
 
         // Recursively evaluate each possible game position for the defined depth (k).
@@ -97,11 +65,6 @@ BScore g9_checkscore(struct connect4* copied, BScore BestScore, int k)
             copied->whoseTurn = (copied->whoseTurn == 'X') ? 'O' : 'X';
 
             //our turn
-
-           if(DEBUG)
-                printf("\n\n\ntempscore: %d vs finalscore: %d turn: %c \n", temp.score, finalsay.score, (turnour == 0) ? 'O' : 'X');
-            if(DEBUG)
-                printf("column: %d vs comparedColumn: %d\n", column, finalsay.column);
             if(turnour == 0)
             {
                 if(finalsay.score < temp.score)
@@ -120,18 +83,12 @@ BScore g9_checkscore(struct connect4* copied, BScore BestScore, int k)
             }
         }
 
-        if(DEBUG)
-                printf("k: %d finalscore now: %d vs Column: %d\n", k, finalsay.score, finalsay.column);
-
-
         // Reset the game board back to its original position
         // in order to test it again.
         copied->board[row][column] = '_';
          BestScore.score += fakescore;
     }
 
-    if(DEBUG)
-        printf("k: %d This is the best column to move: %d with score %d for: %c\n\n", k, finalsay.column, finalsay.score, (turnour == 0) ? 'O' : 'X');
     //finalsay.score += fakescore;
     return finalsay;
 }
@@ -186,22 +143,7 @@ int dxdyEval(struct connect4 *copied, int row, int column, int i, int k)
 
     // If adjacent pieces are next to yours
 	if (k == 1 && currentPos != copied->whoseTurn && currentPos != '_')
-    {
-        /*if(copied->board[row+DX[i]][column+DY[i]] != copied->whoseTurn)
-        {
-            if(copied->board[row+DX[i]*2][column+DY[i]*2] != copied->whoseTurn)
-                return 1000;
-            else return 150;
-        }*/
-
         return /*dxdyEval(copied, row+DX[i], column+DY[i], i, k+1) +*/ 4 + odd;
-    }
-
-    /*
-    if (k == 2 && currentPos != copied->whoseTurn && currentPos != '_')
-		return dxdyEval(copied, row+DX[i], column+DY[i], i, k+1) + 30 + odd;
-    if (k == 3 && currentPos != copied->whoseTurn && currentPos != '_')
-		return dxdyEval(copied, row+DX[i], column+DY[i], i, k+1) + 400 + odd;*/
 
     // If your piece is next to the place pieced, return a power of 7
 	else if (currentPos == copied->whoseTurn)
@@ -213,13 +155,9 @@ int dxdyEval(struct connect4 *copied, int row, int column, int i, int k)
     // If you hit a dead end after moving in a direction for a few,
 	// turn around and count the real score.
     else if (currentPos != copied->whoseTurn && currentPos != '_' && i < 4)
-    {
-    	if(DEBUG) printf("********DX THINGY: %d\n", DX_SIZE-1 -i);
         return dxdyEval(copied, row+DX[i], column+DY[i], DX_SIZE - 1 - i, 0);
-    }
 
 	return pow(7, k-1) + odd;
-
 }
 
 int g9_findscore (struct connect4* copied, BScore BestScore)
@@ -259,11 +197,6 @@ int g9_findscore (struct connect4* copied, BScore BestScore)
     	temp = -1000;
     if (currentPiece == 'O' && (status == X_WINS))
     	temp = -1000;
-
-    if(DEBUG)
-        print_board(copied);
-    if(DEBUG)
-        printf("temp scored a : for this: %d\n", temp);
 
     // If it is our player's turn, return a positive score.
     if (BestScore.ourpiece == copied->whoseTurn) return temp;
@@ -312,7 +245,7 @@ int g9_test_depth(const struct connect4 *game)
 
 
 // After evaluating the best move, return the column to place the piece.
-int g9_moving(const struct connect4 *game, int secondsLeft)
+int g9_move(const struct connect4 *game, int secondsLeft)
 {
     int bestMove;
     bestMove = g9_test_depth(game);
--- Connect4/ourplayer.h
+++ Connect4/ourplayer.h
@@ -2,9 +2,9 @@
 #include "con4lib.h"
 #include <math.h>
 
-#define LOSE -1000
-#define WIN 1000
-#define DEPTH 4
+#define G9_LOSE -1000
+#define G9_WIN 1000
+#define G9_DEPTH 4
 
 // Checks the possible directions
 const int DX_SIZE = 8;
@@ -24,4 +24,4 @@ BScore g9_initstruct(const struct connect4* game);
 BScore g9_checkscore(struct connect4* copied, BScore BestScore, int k);
 int g9_findscore (struct connect4* copied, BScore BestScore);
 int g9_test_depth(const struct connect4 *game);
-int g9_moving(const struct connect4 *game, int secondsLeft);
+int g9_move(const struct connect4 *game, int secondsLeft);
